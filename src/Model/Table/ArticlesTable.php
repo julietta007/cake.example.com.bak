c56d5078f5ae22842c6d00b33e6f5b4d9cd80aed
juliettaurdiales@yahoo.comlsgit statusgit commit README.mdcd /var/www/mtbclsgit config --global user.name juliettaurdiales@yahoo.com <juliettaurdiales@yahoo.com>
2018-01-08 15:03:13 -0600

    Initial commit

---
+++ src/Model/Table/ArticlesTable.php
@@ -0,0 +1,88 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+// in src/Model/Table/ArticlesTable.php
+
+// add this use statement right below the namespace declaration to import
+// the Text class
+use Cake\Utility\Text;
+
+// Add the following method.
+
+/**
+ * Articles Model
+ *
+ * @method \App\Model\Entity\Article get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Article newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Article[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Article|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Article patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Article[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Article findOrCreate($search, callable $callback = null, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class ArticlesTable extends Table
+{
+    public function beforeSave($event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // trim slug to maximum length defined in schema
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+    }
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->belongsTo('Users.Users');
+
+        $this->setTable('articles');
+        $this->setDisplayField('title');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->scalar('title')
+            ->maxLength('title', 255)
+            ->notEmpty('title');
+
+
+            $validator
+                ->scalar('slug')
+                ->maxLength('slug', 191)
+                ->notEmpty('slug');
+
+        $validator
+            ->scalar('body')
+            ->allowEmpty('body');
+
+        return $validator;
+    }
+}
---
+++ src/Template/Articles/index.ctp
@@ -0,0 +1,48 @@
+<h1>Articles</h1>
+<?php if(!empty($this->request->session()->check('Auth.User.id'))): ?>
+    <p><?php echo $this->Html->link("Add Article", ['action' => 'add']) ?></p>
+<?php endif; ?>
+<table>
+    <thead>
+        <tr>
+        <th>title</th>
+        <th>Created</th>
+        <?php if(!empty($this->request->session()->check('Auth.User.id'))): ?>
+            <th>Actions</th>
+        <?php endif; ?>
+    </tr>
+</thead>
+
+<tbody>
+        <?php foreach($articles as $article): ?>
+            <tr>
+                <td>
+        <?php
+        echo $this->Html->link(
+        $article->title,
+    [
+    'action'=>'view',
+    $article->slug
+    ]
+);
+?>
+        </td>
+        <td>
+            <?php echo $article->created->format(DATE_RFC850); ?>
+        </td>
+        <td>
+            <?php if(!empty($this->request->session()->check('Auth.User.id'))); ?>
+        <td>
+            <?php echo $this->Html->link('Edit', ['action'=>'edit', $article->slug]); ?>
+            <?php echo $this->Form->postLink(
+                'Delete',
+                ['action' => 'delete', $article->slug],
+                ['confirm' => 'Are you sure?'])
+            ?>
+        </td>
+    </tr>
+        <?php endforeach; ?>
+
+    </tbody>
+
+</table>
